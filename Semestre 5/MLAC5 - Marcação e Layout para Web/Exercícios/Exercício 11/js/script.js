c4fdaa97a3dbfdcc394fd1f7d38e0c4042c2c770
VICTOR RAMOS <bv3026191@ad.sbv.ifsp.edu.br>
2024-07-31 14:39:28 -0300

    Exercicio 11 - HTML

---
+++ Semestre 5/MLAC5 - Marcação e Layout para Web/Exercícios/Exercício 11/js/script.js
@@ -0,0 +1,49 @@
+$( document ).ready( function() {
+
+    $("#changeColorForm").submit( function(e) {
+        e.preventDefault();
+        $("body").css("background-color", $("#color").val() );
+    })
+
+    $("#emailForm").submit( function (e) {
+
+        if ( isValidEmail( $("#email").val() ) ) {
+            alert("Email inserido corretamente");
+        } else {
+            alert("Email não inserido corretamente");
+            e.preventDefault();
+        }
+
+    })
+
+    $("#changeVisibilityButton").click( function() {
+        
+        if ( $("#title").css("visibility") == "visible"){
+            $("#title").css("visibility", "hidden");
+        } else {
+            $("#title").css("visibility", "visible");
+        }
+
+    })
+
+    $("#changeStyleButton").click( function() {
+
+        if( $("#title").hasClass("class1") ) {
+            $("#title").removeClass("class1")
+            $("#title").addClass("class2")
+        } else {
+            $("#title").removeClass("class2")
+            $("#title").addClass("class1")
+        }
+
+        
+
+    })
+
+})
+
+function isValidEmail( email ) { // Função simples de validação de e-mail
+
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    
+}
